24a0b136aeeaa4b3393e99d6f8bcdc14382ee1f3
Malkeet singh <sudan_malkeet@hotmail.com>
2017-08-11 02:13:32 +0530

    Get /todo route

--- server/server.js
+++ server/server.js
@@ -1,30 +1,46 @@
-var express=require("express");
-var bodyParser=require("body-parser"); 
+var express = require("express");
+var bodyParser = require("body-parser");
 
 
-var{Mongoose}=require("./db/mongoose");
-var{Todo}=require("./models/todo");
-var{User}=require("./models/user");
+var {
+    Mongoose
+} = require("./db/mongoose");
+var {
+    Todo
+} = require("./models/todo");
+var {
+    User
+} = require("./models/user");
 
-var app=express();
+var app = express();
 
-app.listen(3000,(msg)=>{
+app.listen(3000, (msg) => {
     console.log("Server started at 3000");
 });
 
 app.use(bodyParser.json());
 
-app.post("/todo",(req,res)=>{
-    var todo=new Todo({
-        text:req.body.text,
-        completed:req.body.completed
+app.post("/todo", (req, res) => {
+    var todo = new Todo({
+        text: req.body.text,
+        completed: req.body.completed
     });
     todo.save().then(
-        (doc)=>{
+        (doc) => {
             res.send(doc);
         },
-        (e)=>{
+        (e) => {
             res.status(400).send(e);
         }
-    )
-})
+    );
+});
+app.get("/todo",(req,res)=>{
+    Todo.find().then(
+        (doc)=>{
+           res.send({doc});
+        },
+        (err)=>{
+        res.status(400).send(err);
+        }
+    );
+});
